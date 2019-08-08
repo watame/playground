3f1209269e2333d8a76313827da05ad126ced6ff
watame <pandora.border@gmail.com>
2019-08-09 00:14:25 +0900

    Angular Material の Table を追加し、利用時に気を付けることをコメントとして残しました。

--- angular/material/src/app/app-routing.module.ts
+++ angular/material/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: './', component: AppComponent},
+  {path: 'table', component: TableComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/material/src/app/app.module.ts
+++ angular/material/src/app/app.module.ts
@@ -5,14 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { TableComponent } from './table/table.component'; 
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatInputModule } from '@angular/material/input'; 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
