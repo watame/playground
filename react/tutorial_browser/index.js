68698faac59fa10aeafb13d2c415600312e087c2
watame <pandora.border@gmail.com>
2020-05-20 22:35:14 +0900

    ゲームを完成させる：Stateのリフトアップ までのログ

--- react/tutorial_browser/index.js
+++ react/tutorial_browser/index.js
@@ -1,8 +1,12 @@
+// 自身のstateを監理しないコンポーネント = 制御されたコンポーネント
 class Square extends React.Component {
   render() {
     return (
-      // 呼び出し時に格納された変数にアクセスできる
-      <button className="square">
+      // 呼び出し時に格納された変数にもアクセスできる
+      // クリックされるまで無名関数は実行されない
+      // this.setStateを呼び出すことで、再描画をしている
+      // クリックイベントのタイミングでBoardのOnClickで定義した関数が発火する
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.props.value}
       </button>
     );
@@ -10,9 +14,37 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-  // Squareコンポーネント呼び出し時に変数をいれる
+  // コンポーネントが何かを「覚える」ためにはstateを利用する
+  constructor(props) {
+    // JavaScriptのクラスでは、サブクラスのコンストラクタ定義する際は常にsuperがいる
+    // Reactのクラスコンポーネントでは、全コンストラクタをsuper(props)から始める
+    super(props);
+    // プライベートなので、子コンポーネントから変更することはできない
+    this.state = {
+      // 盤面をnullで埋める 
+      squares: Array(9).fill(null),
+    };
+  }
+  
+  // クリック時のイベント定義
+  handleClick(i) {
+    // 配列をコピー
+    const squares = this.state.squares.slice();
+    // コピーした配列の中身を変更
+    squares[i] = 'X';
+    // コピーした配列を代入し、コンポーネントの再描画をする
+    this.setState({squares: squares});
+  }
+  
+  // Squareコンポーネント呼び出し、マス目を描画する
   renderSquare(i) {
-    return <Square value={i} />;
+    // stateの値をpropsとしてコンポーネントに渡す
+    return (<Square
+              value={this.state.squares[i]}
+              // onClickをpropsとして渡している
+              onClick={() => this.handleClick(i)}
+              />
+           );
   }
 
   render() {
