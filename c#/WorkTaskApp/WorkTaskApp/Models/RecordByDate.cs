9b0b4eae37324e988b95d55869020a73029c5ea1
watame <pandora.border@gmail.com>
2020-06-23 01:13:38 +0900

    DB登録処理用のモデルを作成

---
+++ c#/WorkTaskApp/WorkTaskApp/Models/RecordByDate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace WorkTaskApp.Models
+{
+    /// <summary>
+    /// 日付単位レコードクラス
+    /// </summary>
+    class RecordByDate : BindableBase
+    {
+        /// <summary>
+        /// 作業日時
+        /// </summary>
+        private DateWeather dateWeather;
+        public DateWeather DateWeather
+        {
+            get { return dateWeather; }
+            set { SetProperty(ref dateWeather, value); }
+        }
+
+        /// <summary>
+        /// 作業内容インスタンスリスト
+        /// </summary>
+        private ObservableCollection<WorkContent> workContents;
+        public ObservableCollection<WorkContent> WorkContents 
+        {
+            get { return workContents; }
+            set { SetProperty(ref workContents, value); }
+        }
+    }
+}
