3b75bfc1a65e38ecdbf388cb6f4634b29e8da576
watame <pandora.border@gmail.com>
2021-02-09 22:17:37 +0900

    統計データを使うことによってわかりにくくなっていたのでデータ自体を削除

--- rust/sort_test/src/main.rs
+++ rust/sort_test/src/main.rs
@@ -5,6 +5,9 @@ use std::error::Error;
 use std::path::Path;
 
 /// sort-order-test.csv読み込み用Struct
+/// フィールドはcsvで定義されているカラム名をそのまま利用して定義
+/// PartialEqによる比較
+///     https://stackoverflow.com/questions/26958178/how-do-i-automatically-implement-comparison-for-structs-with-floats-in-rust
 #[derive(Debug, Deserialize, PartialEq)]
 struct Person {
     // No
@@ -31,22 +34,6 @@ impl PartialOrd for Person {
                 .then(self.no.cmp(&other.no)),
         )
     }
-}
-
-/// annual-enterprise-survey-2019-financial-year-provisional-size-bands-csv読み込み用の構造体
-/// フィールドはcsvで定義されているカラム名をそのまま利用して定義
-/// PartialEqによる比較
-///     https://stackoverflow.com/questions/26958178/how-do-i-automatically-implement-comparison-for-structs-with-floats-in-rust
-#[derive(Debug, Deserialize, PartialEq)]
-#[allow(non_snake_case)]
-struct Survey {
-    year: i32,
-    industry_code_ANZSIC: String,
-    industry_name_ANZSIC: String,
-    rme_size_grp: String,
-    variable: String,
-    value: String,
-    unit: String,
 }
 
 /// CSVファイルを読み込み、各行をTのStructにパースしたVectorを生成する
@@ -89,28 +76,25 @@ where
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    const FILE_NAME: &str =
-        "annual-enterprise-survey-2019-financial-year-provisional-size-bands-csv.csv";
+    const FILE_NAME: &str = "sort-order-test.csv";
     let target_path = Path::new("./rsc/").join(FILE_NAME);
 
     // csvファイルの読み込み
-    let mut survey_list_sort_by: Vec<Survey> = target_csv_to_parse_struct(&target_path)?;
+    let mut survey_list_sort_by: Vec<Person> = target_csv_to_parse_struct(&target_path)?;
     survey_list_sort_by.sort_by(|a, b| {
-        a.value
-            .cmp(&b.value)
-            .then(a.variable.cmp(&b.variable).then(a.unit.cmp(&b.unit)))
+        a.age
+            .cmp(&b.age)
+            .then(a.name.cmp(&b.name))
+            .then(a.job.cmp(&b.job))
+            .then(a.no.cmp(&b.no))
     });
 
     // csvファイルの読み込み
-    let mut survey_list_is_sorted_by: Vec<Survey> = target_csv_to_parse_struct(&target_path)?;
+    let mut survey_list_is_sorted_by: Vec<Person> = target_csv_to_parse_struct(&target_path)?;
     survey_list_is_sorted_by.sort_by_key(|a| {
         // タプルで比較順を指定する
         // 参考：https://users.rust-lang.org/t/solved-advanced-sort-on-vector-of-slice/13477/9
-        (
-            a.value.to_string(),
-            a.variable.to_string(),
-            a.unit.to_string(),
-        )
+        (a.age, a.name.to_string(), a.job.to_string(), a.no)
     });
 
     // sort_by, sort_by_keyで同一の並びになっているか確認
@@ -124,149 +108,26 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("{:?}", record);
     }
 
+    let answer_target_path = Path::new("./test/normal/sort-order-test_sorted.csv");
+    let survey_list_is_sorted: Vec<Person> = target_csv_to_parse_struct(&answer_target_path)?;
+
+    // ソート済みのCSVを読み込んだ結果とソートした結果が同一か確認
+    for record in survey_list_is_sorted_by
+        .iter()
+        .zip(survey_list_is_sorted.iter())
+    {
+        if record.0 == record.1 {
+            continue;
+        }
+        println!("{:?}", record);
+    }
+
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_struct_is_equal_normal() {
-        // 比較用Struct
-        let survey_a = Survey {
-            year: 1,
-            industry_code_ANZSIC: "ABC".to_string(),
-            industry_name_ANZSIC: "DEF".to_string(),
-            rme_size_grp: "GHI".to_string(),
-            variable: "JKL".to_string(),
-            value: "MNO".to_string(),
-            unit: "PQR".to_string(),
-        };
-
-        // survey_aと同内容のデータを保持しているStruct
-        let survey_b = Survey {
-            year: 1,
-            industry_code_ANZSIC: "ABC".to_string(),
-            industry_name_ANZSIC: "DEF".to_string(),
-            rme_size_grp: "GHI".to_string(),
-            variable: "JKL".to_string(),
-            value: "MNO".to_string(),
-            unit: "PQR".to_string(),
-        };
-        assert!(survey_a == survey_b);
-    }
-
-    #[test]
-    fn test_struct_is_equal_error() {
-        // 比較用Struct
-        let survey_a = Survey {
-            year: 1,
-            industry_code_ANZSIC: "ABC".to_string(),
-            industry_name_ANZSIC: "DEF".to_string(),
-            rme_size_grp: "GHI".to_string(),
-            variable: "JKL".to_string(),
-            value: "MNO".to_string(),
-            unit: "PQR".to_string(),
-        };
-
-        // survey_aとyearのみが異なるStruct
-        let diff_year = Survey {
-            year: 2,
-            industry_code_ANZSIC: "ABC".to_string(),
-            industry_name_ANZSIC: "DEF".to_string(),
-            rme_size_grp: "GHI".to_string(),
-            variable: "JKL".to_string(),
-            value: "MNO".to_string(),
-            unit: "PQR".to_string(),
-        };
-
-        // survey_aとindustry_codeのみが異なるStruct
-        let diff_code = Survey {
-            year: 1,
-            industry_code_ANZSIC: "XYZ".to_string(),
-            industry_name_ANZSIC: "DEF".to_string(),
-            rme_size_grp: "GHI".to_string(),
-            variable: "JKL".to_string(),
-            value: "MNO".to_string(),
-            unit: "PQR".to_string(),
-        };
-
-        // survey_aとindustry_nameのみが異なるStruct
-        let diff_name = Survey {
-            year: 1,
-            industry_code_ANZSIC: "ABC".to_string(),
-            industry_name_ANZSIC: "XYZ".to_string(),
-            rme_size_grp: "GHI".to_string(),
-            variable: "JKL".to_string(),
-            value: "MNO".to_string(),
-            unit: "PQR".to_string(),
-        };
-
-        // survey_aとsize_grpのみが異なるStruct
-        let diff_size = Survey {
-            year: 1,
-            industry_code_ANZSIC: "ABC".to_string(),
-            industry_name_ANZSIC: "DEF".to_string(),
-            rme_size_grp: "XYZ".to_string(),
-            variable: "JKL".to_string(),
-            value: "MNO".to_string(),
-            unit: "PQR".to_string(),
-        };
-
-        // survey_aとvariableのみが異なるStruct
-        let diff_variable = Survey {
-            year: 1,
-            industry_code_ANZSIC: "ABC".to_string(),
-            industry_name_ANZSIC: "DEF".to_string(),
-            rme_size_grp: "GHI".to_string(),
-            variable: "XYZ".to_string(),
-            value: "MNO".to_string(),
-            unit: "PQR".to_string(),
-        };
-
-        // survey_aとvalueのみが異なるStruct
-        let diff_value = Survey {
-            year: 1,
-            industry_code_ANZSIC: "ABC".to_string(),
-            industry_name_ANZSIC: "DEF".to_string(),
-            rme_size_grp: "GHI".to_string(),
-            variable: "JKL".to_string(),
-            value: "XYZ".to_string(),
-            unit: "PQR".to_string(),
-        };
-
-        // survey_aとunitのみが異なるStruct
-        let diff_unit = Survey {
-            year: 1,
-            industry_code_ANZSIC: "ABC".to_string(),
-            industry_name_ANZSIC: "DEF".to_string(),
-            rme_size_grp: "GHI".to_string(),
-            variable: "JKL".to_string(),
-            value: "MNO".to_string(),
-            unit: "XYZ".to_string(),
-        };
-
-        // 全てが異なるstruct
-        let diff_all = Survey {
-            year: 2,
-            industry_code_ANZSIC: "abc".to_string(),
-            industry_name_ANZSIC: "def".to_string(),
-            rme_size_grp: "ghi".to_string(),
-            variable: "jkl".to_string(),
-            value: "mno".to_string(),
-            unit: "pqr".to_string(),
-        };
-
-        assert_eq!(false, survey_a == diff_year);
-        assert_eq!(false, survey_a == diff_code);
-        assert_eq!(false, survey_a == diff_name);
-        assert_eq!(false, survey_a == diff_size);
-        assert_eq!(false, survey_a == diff_variable);
-        assert_eq!(false, survey_a == diff_value);
-        assert_eq!(false, survey_a == diff_unit);
-        assert_eq!(false, survey_a == diff_all);
-    }
 
     #[test]
     fn test_is_csv_sorted_by_define_priority_normal() {
@@ -285,7 +146,7 @@ mod tests {
             // 定義した優先度で各フィールドの比較処理を行い、1つ前の要素が大きければエラーとする
             if person_tuple[0] > person_tuple[1] {
                 result = false;
-                break; 
+                break;
             }
         }
         assert!(result);
@@ -308,7 +169,7 @@ mod tests {
             // 定義した優先度で各フィールドの比較処理を行い、1つ前の要素が大きければエラーとする
             if person_tuple[0] > person_tuple[1] {
                 result = false;
-                break; 
+                break;
             }
         }
         assert_eq!(false, result);
